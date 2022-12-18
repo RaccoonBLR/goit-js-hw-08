fe0ac6d821b01ba4e1d711186af971ea78a10d1d
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2022-12-18 23:10:13 +0200

    Initial commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from "./gallery-items";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", preventDefaultAction);
+
+initializationOfGalleryMarkup(createGalleryMarkup(galleryItems));
+
+function createElementOfMarkup(Item) {
+  const { preview, original, description } = Item;
+
+  return `<a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+           </a>`;
+}
+
+function createGalleryMarkup(galleryMarkupItems) {
+  const galleryMarkup = galleryMarkupItems
+    .map((galleryItem) => createElementOfMarkup(galleryItem))
+    .join("");
+
+  return galleryMarkup;
+}
+
+function initializationOfGalleryMarkup(markup) {
+  gallery.insertAdjacentHTML("beforeend", markup);
+
+  initializationSimpleLightbox();
+}
+
+function preventDefaultAction(Event) {
+  Event.preventDefault();
+}
+
+function initializationSimpleLightbox() {
+  let lightbox = new SimpleLightbox(".gallery .gallery__item", {
+    scrollZoom: false,
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
---
+++ src/js/03-feedback.js
@@ -0,0 +1,79 @@
+import throttle from "lodash.throttle";
+
+const feedbackFormState = {};
+
+const formRef = document.querySelector(".feedback-form");
+
+formRef.addEventListener("input", throttle(storageUpdate, 500));
+formRef.addEventListener("submit", handleSubmitForm);
+
+storageStatusCheck();
+
+function storageStatusCheck() {
+  const currentValue = getFormCurrentValue();
+
+  if (currentValue?.email) {
+    formRef.email.value = currentValue.email;
+  }
+
+  if (currentValue?.message) {
+    formRef.message.value = currentValue.message;
+  }
+
+  return;
+}
+
+function storageUpdate(event) {
+  if (event.target.name === "email") {
+    feedbackFormState.email = `${event.target.value}`;
+
+    localStorage.setItem(
+      "feedback-form-state",
+      JSON.stringify(feedbackFormState)
+    );
+  }
+
+  if (event.target.name === "message") {
+    feedbackFormState.message = `${event.target.value}`;
+
+    localStorage.setItem(
+      "feedback-form-state",
+      JSON.stringify(feedbackFormState)
+    );
+  }
+
+  return;
+}
+
+function handleSubmitForm(event) {
+  event.preventDefault();
+
+  formDataOutput();
+  clearInputFields(event);
+  clearFormDataStorage();
+}
+
+function getFormCurrentValue() {
+  return JSON.parse(localStorage.getItem("feedback-form-state"));
+}
+
+function formDataOutput() {
+  if (getFormCurrentValue()) {
+    console.log(getFormCurrentValue());
+  }
+}
+
+function clearInputFields(event) {
+  if (event.target.email.value) {
+    event.target.email.value = "";
+  }
+  if (event.target.message.value) {
+    event.target.message.value = "";
+  }
+}
+
+function clearFormDataStorage() {
+  if (localStorage.getItem("feedback-form-state")) {
+    localStorage.removeItem("feedback-form-state");
+  }
+}
